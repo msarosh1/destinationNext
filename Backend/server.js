833971dc2f41f2592b69c25831ac42c9b8f62ee2
msarosh1 <muhammadsarosh@hotmail.com>
2022-10-12 18:24:03 +0500

    Deployment testing 15

--- Backend/server.js
+++ Backend/server.js
@@ -27,10 +27,17 @@ const port = process.env.PORT || 5000;
 //for environment vars
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connected successfully!");
+  })
+  .catch((err) => {
+    console.log("Error connecting with error code:", err);
+  });
 
 //passport local strategy
 const LocalStrategy = require("passport-local").Strategy;
