b4dcd38752421a3c822f1cccd7d6ccddaac85df5
msarosh1 <muhammadsarosh@hotmail.com>
2022-10-13 19:26:05 +0500

    Deployment testing 22

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const app = express(); // server software
 const bodyParser = require("body-parser"); // parser middleware
+const path = require("path");
 
-const cors = require("cors");
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+const buildPath = path.join(__dirname, "..", "build");
+app.use(express.static(buildPath));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
